df27f34851b5e424c1c3a7031251c46d61b0b7d9
Niktib <nikolas.otto.maier@gmail.com>
2018-04-07 00:11:53 -0400

    Add files via upload

---
+++ WindowsFormsApplication5/WindowsFormsApplication5/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WindowsFormsApplication5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Troll_Toad = new System.Windows.Forms.RadioButton();
+            this.TCG = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(31, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(241, 138);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(310, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(221, 160);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(310, 178);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(105, 160);
+            this.listBox2.TabIndex = 2;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(421, 178);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(110, 160);
+            this.listBox3.TabIndex = 2;
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(238, 66);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Troll_Toad
+            // 
+            this.Troll_Toad.AutoSize = true;
+            this.Troll_Toad.Checked = true;
+            this.Troll_Toad.Location = new System.Drawing.Point(63, 28);
+            this.Troll_Toad.Name = "Troll_Toad";
+            this.Troll_Toad.Size = new System.Drawing.Size(94, 17);
+            this.Troll_Toad.TabIndex = 4;
+            this.Troll_Toad.TabStop = true;
+            this.Troll_Toad.Text = "Troll and Toad";
+            this.Troll_Toad.UseVisualStyleBackColor = true;
+            // 
+            // TCG
+            // 
+            this.TCG.AutoSize = true;
+            this.TCG.Location = new System.Drawing.Point(166, 28);
+            this.TCG.Name = "TCG";
+            this.TCG.Size = new System.Drawing.Size(47, 17);
+            this.TCG.TabIndex = 4;
+            this.TCG.TabStop = true;
+            this.TCG.Text = "TCG";
+            this.TCG.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 348);
+            this.Controls.Add(this.TCG);
+            this.Controls.Add(this.Troll_Toad);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton Troll_Toad;
+        private System.Windows.Forms.RadioButton TCG;
+    }
+}
+
---
+++ WindowsFormsApplication5/WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Collections;
+
+namespace WindowsFormsApplication5
+{
+    public partial class Form1 : Form
+    {
+        public struct Card
+        {
+            public string name;
+            public double price;
+            public string set;
+            public bool Foil;
+            public bool ReverseHolo;
+        }
+        public struct Set
+        {
+            public string name;
+            public int setNum;
+            public string URL;
+            public void URLCreation()
+            {
+                string test;
+
+                if (name.Contains('-')) { test = name.Substring(0, 5).Replace(" ", "") + name.Substring(6).Replace(" ", "-"); }
+                else { test = name.Replace(" ", "-"); }
+                this.URL = test.Replace(":", "");
+            }
+        }
+        public List<Set> lSets;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int TTorTCG;
+            string data = "";
+            string SetNameTemp, SetNameHolder;
+
+            string[] ttarray = new string[] { "https://www.trollandtoad.com/Pokemon/7061.html", "inline smallFont subCats", "<a onclick='openSets()' class='seeAllCats'>" };
+            string[] tcgArray = new string[] { "https://shop.tcgplayer.com/pokemon", "<select id=\"SetName\" name=\"SetName\">", "<div class=\"SearchParameter Floating\">" };
+            string[][] BothWebsites = { ttarray, tcgArray };
+            if (Troll_Toad.Checked) { TTorTCG = 0; } else { TTorTCG = 1; }
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(BothWebsites[TTorTCG][0]);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = null;
+
+                lSets = new List<Set>();
+                Set CurrentSet = new Set();
+                int i = 1;
+                if (response.CharacterSet == null) { readStream = new StreamReader(receiveStream); }
+                else { readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet)); }
+                while ((data = readStream.ReadLine()).Contains(BothWebsites[TTorTCG][1]) == false) { }
+                while ((data = readStream.ReadLine()).Contains(BothWebsites[TTorTCG][2]) == false)
+                {
+                    try
+                    {
+                        if (Troll_Toad.Checked)
+                        {
+                            SetNameHolder = data.Trim().Substring(4);
+                            if (SetNameHolder.Contains("inline smallFont subCats")) { SetNameHolder = SetNameHolder.Substring(48); }
+                            if (SetNameHolder.Contains("subCatAlphaHeader")) { SetNameHolder = SetNameHolder.Substring(36); }
+                            SetNameTemp = "";
+                            SetNameTemp = SetNameHolder.Split('>')[1].Trim(); CurrentSet.name = SetNameTemp.Substring(0, SetNameTemp.Length - 3);
+                            CurrentSet.URL = data.Split('\'')[1];
+                        }
+                        else { CurrentSet.name = data.Replace("\"", "#").Replace("&amp;", "&").Split('#')[1]; CurrentSet.URLCreation(); }
+
+                        CurrentSet.setNum = i++;
+                        lSets.Add(CurrentSet);
+                    }
+                    catch { }
+                }
+
+                response.Close();
+                readStream.Close();
+
+                listBox1.BeginUpdate();
+                for (int j = i - 2; j > 0; j--)
+                {
+                    listBox1.Items.Add(lSets[j].name);
+                }
+                listBox1.EndUpdate();
+                MessageBox.Show(lSets[i-8].URL);
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int TTorTCG;
+            string data = "";
+            string SetNameTemp, SetNameHolder, URL;
+            int x = 0;
+            Set TempSet;
+
+            string[] ttarray = new string[] { "https://www.trollandtoad.com", "items. Showing items ", " < div class=\"catResultPages\">" };
+            string[] tcgArray = new string[] { "https://shop.tcgplayer.com/pokemon", "<select id=\"SetName\" name=\"SetName\">", "<div class=\"SearchParameter Floating\">" };
+            string[][] BothWebsites = { ttarray, tcgArray };
+            if (Troll_Toad.Checked) { TTorTCG = 0; } else { TTorTCG = 1; }
+            SetNameHolder = listBox1.SelectedItem.ToString();
+            var es = new SetSearch(SetNameHolder);
+            List<Card> lCard;
+            //lSets[lSets.FindIndex(es.Matching)].URL
+            
+            for (int i = 1; i < 20; i++) //Webpage
+            {
+                URL = BothWebsites[TTorTCG][0] + lSets[lSets.FindIndex(es.Matching)].URL + "?sois=No&pageLimiter=100&showImage=No&PageNum=" + i;
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    #region //Stupid setting up stream stuff
+                    Stream receiveStream = response.GetResponseStream();
+                    StreamReader readStream = null;
+
+                    if (response.CharacterSet == null) { readStream = new StreamReader(receiveStream); }
+                    else { readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet)); }
+                    #endregion 
+                    string failState = "Sorry... Your search has come back with no results. Please try a different combination of search criteria.";
+                    data = readStream.ReadLine();
+                    while (!data.Contains(failState)  )
+                    while ((data = readStream.ReadLine()).Contains(BothWebsites[TTorTCG][1]) == false) { }
+                    //+ x + 1 + " - " + x + 100
+                    while ((data = readStream.ReadLine()).Contains(BothWebsites[TTorTCG][2]) == false)
+                    {
+                        try { listBox1.Items.Add(data.Replace("\"", "&").Split('&')[1]); }
+                        catch { }
+                    }
+
+                    response.Close();
+                    readStream.Close();
+                }
+                else
+                {
+                    MessageBox.Show("dis shit is fucked up");
+                }
+            }
+            
+        }
+
+
+
+        public class SetSearch
+        {
+            String _s;
+            public SetSearch(String s) { _s = s; }
+            public bool Matching(Set e) { return e.name.StartsWith(_s, StringComparison.InvariantCultureIgnoreCase); }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
